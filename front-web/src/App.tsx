ef0012b89a2002f25f60b49139a035a82a4bcb4e
Vitor Siwerski Aronque <vitorsiwerskiaronque@gmail.com>
2020-09-16 10:31:28 -0300

    Adicionando componente header

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
-    <h1>Semana dev superior</h1>
+    <div>
+      <Header />
+    </div>
   );
 }
 
