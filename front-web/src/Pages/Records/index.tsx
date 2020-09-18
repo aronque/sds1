6a64c71b20dcc82acdc8e03a9826c723e2e1e2f1
Vitor Siwerski Aronque <vitorsiwerskiaronque@gmail.com>
2020-09-18 13:31:51 -0300

    Configurando front-web para produção

--- front-web/src/Pages/Records/index.tsx
+++ front-web/src/Pages/Records/index.tsx
@@ -4,9 +4,9 @@ import axios from 'axios';
 import { RecordsResponse } from './types';
 import { formatDate } from './helpers';
 import Pagination from './Pagination';
-import Filters from '../../Components/Filters' 
+import Filters from '../../Components/Filters'; 
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://sds1-vitor.herokuapp.com';
 
 const Records = () => {
     
